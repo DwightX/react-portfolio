dfca105b41fbe7d57ee4ceec5d56525bf92b2cb5
Dwight <dwight@Marketing-1-iMac.local>
2021-10-29 16:29:47 -0400

    added blog using insanity sanity

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import Navbar from './components/Navbar'
 import './index.css';
 import Maincontent from './components/Maincontent';
+import { BrowserRouter, Route } from "react-router-dom";
+import AllPosts from "./components/AllPosts.js";
+import OnePost from "./components/OnePost.js";
 import useMediaQuery from '@mui/material/useMediaQuery';
 
 
@@ -9,10 +12,16 @@ function App() {
   const matches = useMediaQuery('(max-width:768px)');
 
   return (
+    <BrowserRouter>
     <div className="App">
+        <Route component={AllPosts} path="/blog" exact />
+        <Route component={OnePost} path="/:slug" exact/>
+        <Route path="/" exact> 
       { matches && <Navbar />}
       <Maincontent />
+      </Route>
     </div>
+    </BrowserRouter>
   );
 }
 
