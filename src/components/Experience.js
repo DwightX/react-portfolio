eea5d6f8d1d2ecd9256e2f0744f821efbfe83bbb
Dwight Carter <bigbrother@dwights-mbp.home>
2021-11-02 04:03:57 -0400

    style changes

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -14,7 +14,7 @@ const Experience = props => {
                     <Grid item xs={12} md={6} lg={6}>
                         <div>
                             <h1 className="pageTitles">
-                                <span className='text-6xl'>Experience</span>
+                                <span className='text-8xl'>Experience</span>
                             </h1>
                             <p className="textBlurbs">From working overnight in a call center to being a self taught developer.</p>
                         </div>
@@ -22,15 +22,15 @@ const Experience = props => {
                     </Grid>
                     <Grid item xs={12} md={6} lg={6}>
                         <div className='py-4 textBlurbs'>
-                            <h2><span className='thirdColorBG text-xl px-1'>FRONT END WEB DEVELOPER</span> ZANIBONI LIGHTING</h2>
+                            <h2><span className='thirdColorBG text-2xl px-1'>FRONT END WEB DEVELOPER</span> ZANIBONI LIGHTING</h2>
                             <p>For years, Zaniboni Lighting has been developing a line of products aimed at the us architectural market.</p>
                         </div>
                         <div className='py-4 textBlurbs'>
-                            <h2 className='pb-2'><span className='thirdColorBG text-xl px-1'>JR FRONT END WEB DEVELOPER</span> THE WAVE OF CLEARWATER</h2>
+                            <h2 className='pb-2'><span className='thirdColorBG text-2xl px-1'>JR FRONT END WEB DEVELOPER</span> THE WAVE OF CLEARWATER</h2>
                             <p>The Wave of clearwater offers individualized treatment in an executive level women's only setting for mental and behavioral challenges.</p>
                         </div>
                         <div className='py-4 textBlurbs' >
-                            <h2 className='pb-2'><span className='thirdColorBG text-xl px-1'>JR FRONT END WEB DEVELOPER / CONTENT DEVELOPER</span> H2 IT SOLUTIONS</h2>
+                            <h2 className='pb-2'><span className='thirdColorBG text-2xl px-1'>JR FRONT END WEB DEVELOPER / CONTENT DEVELOPER</span> H2 IT SOLUTIONS</h2>
                             <p>H2-IT solutions is an orlando-based company providing user-friendly, engaging educational and training solutions for the public.</p>
                         </div>
                     </Grid>
